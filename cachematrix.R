17abb88f7a5bf04bf0ec2f7a959b8856230717ec
Yumi <yuweiming9@gmail.com>
2017-07-16 23:35:46 -0500

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My functions creates inverted matrix using R scoping
+## rules and with caching functionality. It helps saving intermediates
+## as inputs for the cached values.
 
-## Write a short comment describing this function
+## Starts with cache matrix(function)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setInverseMatrix <- function(inverseMatrix) inverse <<- inverseMatrix
+  getInverseMatrix <- function() inverse
+  list(set = set,
+       get = get,
+       setInverseMatrix = setInverseMatrix,
+       getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## Then the solve() function gets inverse of the matrix
+## using the makeCacheMatrix() function I created above.
+## Basically this is executed by if there's inverse be calculated already
+## and matrix is not changed, then retrieve inverse from cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverseMatrix() # from cache
+  if (!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get() # get
+  inverse <- solve(data, ...) # solve
+  x$setInverseMatrix(inverse)
+  inverse # return
 }
